6c40ceaf6a9dfcadc0721a6396083802d9a1dc9b
lkl4502 <lkl4502@naver.com>
2023-03-09 02:02:04 +0900

    feature: checklist input 모달창 기본 크기를 window size에 비례하도록 설정

--- app/src/main/java/com/example/haru/view/checklist/ChecklistInputFragment.kt
+++ app/src/main/java/com/example/haru/view/checklist/ChecklistInputFragment.kt
@@ -1,15 +1,20 @@
 package com.example.haru.view.checklist
 
+import android.app.Activity
 import android.app.Dialog
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.findFragment
 import com.example.haru.R
 import com.example.haru.databinding.FragmentChecklistInputBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ChecklistInputFragment : BottomSheetDialogFragment() {
@@ -31,23 +36,42 @@ class ChecklistInputFragment : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-//        return inflater.inflate(R.layout.fragment_checklist_input,container,false)
         binding = FragmentChecklistInputBinding.inflate(inflater)
         return binding.root
     }
 
-//    override fun setupDialog(dialog: Dialog, style: Int) {
-//        super.setupDialog(dialog, style)
-//        val
-//    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
+
+        dialog.setOnShowListener{
+            val bottomSheetDialog = it as BottomSheetDialog
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog){
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from<View>(bottomSheet)
+        val layoutParams = bottomSheet!!.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 85 / 100
+    }
+
+    private fun getWindowHeight(): Int {
+        val displayMetrics : DisplayMetrics = this.resources.displayMetrics
+//        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val btnclose = view.findViewById<Button>(R.id.btn_close)
-//
-//        btnclose.setOnClickListener{
-//            dismiss()
-//        }
+
         binding.btnClose.setOnClickListener{
             dismiss()
         }
