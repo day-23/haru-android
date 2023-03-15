9e472cfca4a4f97e735f446aa971e5481f8d6297
ablecp <tasong12@kookmin.ac.kr>
2023-03-15 20:06:04 +0900

    feature:데이터 타입 추가

--- app/src/main/java/com/example/haru/view/timetable/timetable_data.kt
+++ app/src/main/java/com/example/haru/view/timetable/timetable_data.kt
@@ -1,5 +1,9 @@
 package com.example.haru.view.timetable
 
+import android.graphics.Color
+
 data class timetable_data (
-    val time: String
+    val time: String,
+    var eventName: String= "",
+    var eventColor: Int = Color.TRANSPARENT
     )
