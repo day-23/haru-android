9cb5b9a1a7f5d1852c6ad0d586a36bb0812cbbcb
ablecp <tasong12@kookmin.ac.kr>
2023-03-16 12:35:58 +0900

    feature:date pick 구현

--- app/src/main/java/com/example/haru/view/timetable/TimetableFragment.kt
+++ app/src/main/java/com/example/haru/view/timetable/TimetableFragment.kt
@@ -1,22 +1,32 @@
 package com.example.haru.view.timetable
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.haru.R
+import org.w3c.dom.Text
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class TimetableFragment : Fragment() {
 
     val timetableData: ArrayList<timetable_data> = ArrayList()
     lateinit var recyclerView1: RecyclerView
+    lateinit var timetableMonthTextView: TextView
+    lateinit var timetableYearTextView: TextView
 
     companion object {
         const val TAG: String = "로그"
@@ -38,6 +48,13 @@ class TimetableFragment : Fragment() {
         val timetableAdapter = TimetableAdapter(requireContext(), timetableData)
         Log.d(TAG, "TimetableFragment - onCreateView() called")
         var rootView = inflater.inflate(R.layout.fragment_timetable, container, false)
+
+        timetableMonthTextView = rootView.findViewById<TextView>(R.id.timetable_month)
+        timetableYearTextView = rootView.findViewById<TextView>(R.id.timetable_year)
+
+        timetableMonthTextView.setOnClickListener {
+            showDatePickerDialog()
+        }
 
         timetableData.clear()
         for (i: Int in 1..23) {
@@ -62,7 +79,39 @@ class TimetableFragment : Fragment() {
         itemTouchHelper.attachToRecyclerView(recyclerView1)
 
 
+
         return rootView
         //return inflater.inflate(R.layout.fragment_timetable, container, false)
     }
+    private fun showDatePickerDialog() {
+       val calendar = Calendar.getInstance()
+        val datePicker = DatePicker(requireContext())
+        datePicker.init(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH),
+            null
+        )
+
+        val dialog = androidx.appcompat.app.AlertDialog.Builder(requireContext())
+            .setView(datePicker)
+            .setPositiveButton("apply") {dialog, _ ->
+                val year = datePicker.year
+                val month = datePicker.month + 1
+                val day = datePicker.dayOfMonth
+                val day_of_week = calendar.get(Calendar.DAY_OF_WEEK)
+                val selectedYear = SimpleDateFormat("yyyy년").format(Date(year - 1900, month - 1, day))
+                val selectedMonth = SimpleDateFormat("M월").format(Date(year - 1900, month - 1, day))
+                timetableMonthTextView.text = selectedMonth
+                timetableYearTextView.text = selectedYear
+                Toast.makeText(requireContext(), "${day_of_week}", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }
+            .setNegativeButton("cancel") {dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+        dialog.show()
+    }
+
 }
