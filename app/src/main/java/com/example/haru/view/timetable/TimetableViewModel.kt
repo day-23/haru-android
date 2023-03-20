7f3be25bbf3e3eab683c388806be8ef6d276f776
ablecp <tasong12@kookmin.ac.kr>
2023-03-20 12:19:23 +0900

    feature:timetable 뷰모델 생성

--- app/src/main/java/com/example/haru/view/timetable/TimetableViewModel.kt
+++ app/src/main/java/com/example/haru/view/timetable/TimetableViewModel.kt
@@ -1,47 +1,79 @@
-//package com.example.haru.view.timetable
-//
-//import android.widget.DatePicker
-//import android.widget.Toast
-//import androidx.core.content.ContentProviderCompat.requireContext
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//import java.text.SimpleDateFormat
-//import java.util.*
-//
-//class CalendarViewModel: ViewModel() {
-//
-//    private var _week = MutableLiveData<String>()
-//
-//    val week: LiveData<String>
-//        get() = _week
-//
-//    fun showDatePickerDialog(): {
-//        val calendar = Calendar.getInstance()
-//        val datePicker = DatePicker(requireContext())
-//        datePicker.init(
-//            calendar.get(Calendar.YEAR),
-//            calendar.get(Calendar.MONTH),
-//            calendar.get(Calendar.DAY_OF_MONTH),
-//            null
-//        )
-//
-//        val dialog = androidx.appcompat.app.AlertDialog.Builder(requireContext())
-//            .setView(datePicker)
-//            .setPositiveButton("apply") {dialog, _ ->
-//                val year = datePicker.year
-//                val month = datePicker.month + 1
-//                val day = datePicker.dayOfMonth
-//                val day_of_week = calendar.get(Calendar.DAY_OF_WEEK)
-//                val selectedYear = SimpleDateFormat("yyyy년").format(Date(year - 1900, month - 1, day))
-//                val selectedMonth = SimpleDateFormat("M월").format(Date(year - 1900, month - 1, day))
-//                Toast.makeText(requireContext(), "${day_of_week}", Toast.LENGTH_SHORT).show()
-//                dialog.dismiss()
-//            }
-//            .setNegativeButton("cancel") {dialog, _ ->
-//                dialog.dismiss()
-//            }
-//            .create()
-//        dialog.show()
-//    }
-//}
+package com.example.haru.view.timetable
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.abs
+
+class TimetableViewModel: ViewModel() {
+
+    val timetableData = MutableLiveData<ArrayList<timetable_data>>()
+    val days = MutableLiveData<ArrayList<Int>>()
+    val calendar = Calendar.getInstance()
+
+    init {
+        timetableData.value = ArrayList()
+        days.value = ArrayList()
+    }
+
+    fun getTimetableData() {
+        val data = ArrayList<timetable_data>()
+        for (i: Int in 1..23) {
+            if (i < 12) {
+                if(i == 1)
+                    data.add(timetable_data("${i}\n오전"))
+                else
+                    data.add(timetable_data("${i}"))
+            } else {
+                if (i == 12) {
+                    data.add(timetable_data("12\n오후"))
+                } else {
+                    data.add(timetable_data("${i - 12}\n"))
+                }
+            }
+        }
+        timetableData.value = data
+    }
+
+    fun getDays(year: Int, month: Int, day: Int) {
+        val dayList = ArrayList<Int>()
+        var d = day
+        var lastOfMonth = 0
+
+        calendar.set(year, month - 1, day)
+        lastOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+        calendar.set(year, month, day)
+        val dayOfWeek = SimpleDateFormat("E").format(Date(year - 1900, month, day))
+
+        when(dayOfWeek){
+            "Mon"-> d -= 1
+            "Tue"-> d -= 2
+            "Wed"-> d -= 3
+            "Thu"-> d -= 4
+            "Fri"-> d -= 5
+            "Sat"-> d -= 6
+            "Sun"-> d
+        }
+
+        for (i: Int in 1 .. 7){
+            if(d < 1){
+                dayList.add(lastOfMonth - abs(d))
+            }
+            else if(d > lastOfMonth){
+                dayList.add(d - lastOfMonth)
+            }
+            else{
+                dayList.add(d)
+            }
+            d += 1
+        }
+        days.value = dayList
+    }
+
+    fun updateMonth(year: Int, month: Int) {
+        calendar.set(year, month - 1, 1)
+        getDays(year, month, 1)
+        getTimetableData()
+    }
+}
