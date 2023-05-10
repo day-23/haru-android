ae9603510b361011a8440f2c9bc58af6447dd5f5
오홍석 <lkl4502@kookmin.ac.kr>
2023-05-11 04:01:42 +0900

    feature: Custom Time Picker 시간 변경시 오전 오후 자동 변경

--- app/src/main/java/com/example/haru/view/customDialog/CustomTimeDialog.kt
+++ app/src/main/java/com/example/haru/view/customDialog/CustomTimeDialog.kt
@@ -1,5 +1,6 @@
 package com.example.haru.view.customDialog
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Point
@@ -15,6 +16,7 @@ import android.view.WindowManager
 import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.example.haru.R
 import com.example.haru.databinding.CustomTimePickerBinding
 import com.example.haru.utils.FormatDate
 import java.util.*
@@ -23,9 +25,10 @@ class CustomTimeDialog(date: Date? = null) : DialogFragment() {
     private lateinit var binding: CustomTimePickerBinding
     private var hourList: Array<String> = Array(12) { "" }
     private var minuteList: Array<String> = Array(12) { "" }
+    private var timeList: Array<String> = Array(2) { "" }
     private var startHour: Int = 11
     private var startMinute: Int = 0
-    private var flag = false   // false이면 오전, true이면 오후
+    private var flag = 1   // 0이면 오전, 1이면 오후
 
     interface TimePickerClickListener {
         fun onClick(hourNumberPicker: NumberPicker, minuteNumberPicker: NumberPicker)
@@ -44,27 +47,27 @@ class CustomTimeDialog(date: Date? = null) : DialogFragment() {
                 startMinute -= remain
             else startMinute += (5 - remain)
             startMinute /= 5
-            if (startHour > 12){
-            // 오후로 설정 하고 index = startHour % 12 - 1
-                startHour = startHour % 12 -1
-                flag = true
-            }
-            else if (startHour == 12) {
-            // index = 11 오후로 설정
+            if (startHour > 12) {
+                // 오후로 설정 하고 index = startHour % 12 - 1
+                startHour = startHour % 12 - 1
+                flag = 1
+            } else if (startHour == 12) {
+                // index = 11 오후로 설정
                 startHour = 11
-                flag = true
-            }
-            else if (startHour == 0) {
+                flag = 1
+            } else if (startHour == 0) {
                 // 오전으로 설정하고 index = 11
                 startHour = 11
-                flag = false
-            }
-            else if (startHour < 12){
-            // indxe = startHour - 1 오전으로 설정
+                flag = 0
+            } else if (startHour < 12) {
+                // indxe = startHour - 1 오전으로 설정
                 startHour -= 1
-                flag = false
+                flag = 0
             }
         }
+
+        timeList[0] = "오전"
+        timeList[1] = "오후"
 
         for (i in 0 until 12) {
             val hour = (i + 1).toString()
@@ -99,12 +102,40 @@ class CustomTimeDialog(date: Date? = null) : DialogFragment() {
 
         Toast.makeText(requireContext(), "Custom Time Picker", Toast.LENGTH_SHORT).show()
 
+        binding.timeDivision.apply {
+            descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            minValue = 0
+            maxValue = 1
+            value = flag
+            displayedValues = timeList
+            wrapSelectorWheel = false
+
+        }
+//        binding.timeDivision.
+
         binding.npHourPick.apply {
             descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
             minValue = 0
             maxValue = 11
             value = startHour
             displayedValues = hourList
+            setOnValueChangedListener { numberPicker, oldVal, newVal ->
+                if ((oldVal == 0 && newVal == 11) || (oldVal == 11 && newVal == 0)){
+                    if (binding.timeDivision.value == 1)
+                        binding.timeDivision.value = 0
+                    else binding.timeDivision.value = 1
+//                    if (binding.timeDivision.value == 0){
+//                        animateNumberPicker(binding.timeDivision, binding.timeDivision.height, binding.timeDivision.height + binding.timeDivision.height / 3)
+//                        binding.timeDivision.value = 1
+//                    }
+//                    else{
+//                        animateNumberPicker(binding.timeDivision, binding.timeDivision.height, binding.timeDivision.height - binding.timeDivision.height / 3)
+//                        binding.timeDivision.value = 0
+//                    }
+//                    this.value = 1
+                    Log.d("20191627", "스크롤")
+                }
+            }
         }
 
         binding.npMinutePick.apply {
@@ -160,4 +191,13 @@ class CustomTimeDialog(date: Date? = null) : DialogFragment() {
             window?.setLayout(x, y)
         }
     }
+
+//    private fun animateNumberPicker(numberPicker: NumberPicker, start: Int, end: Int) {
+//        val valueAnimator = ValueAnimator.ofInt(start, end)
+//        valueAnimator.duration = 400
+//        valueAnimator.addUpdateListener {
+//            numberPicker.translationY = it.animatedValue as Float
+//        }
+//        valueAnimator.start()
+//    }
 }
