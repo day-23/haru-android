fa43b616c0786bb0b731139392222a6b87bf6ca1
오홍석 <lkl4502@kookmin.ac.kr>
2023-05-20 02:05:49 +0900

    feature : 카카오 해시키 확인

--- app/src/main/java/com/example/haru/App.kt
+++ app/src/main/java/com/example/haru/App.kt
@@ -2,7 +2,9 @@ package com.example.haru
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class App : Application() {
     companion object {
@@ -17,6 +19,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        /* 디버그 설정을 위한 key url */
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
 
         // Kakao SDK 초기화
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
