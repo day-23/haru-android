28cfb814aa25355c0512c975412c4c43bfd70b37
minleejae <mmj9808@naver.com>
2023-05-29 03:37:24 +0900

    fix : 드래그 드랍 안빠져나가게 수정

--- app/src/main/java/com/example/haru/view/timetable/ScheduleDraglistener.kt
+++ app/src/main/java/com/example/haru/view/timetable/ScheduleDraglistener.kt
@@ -2,8 +2,10 @@ package com.example.haru.view.timetable
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.LayerDrawable
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.DragEvent
 import android.view.View
@@ -11,99 +13,125 @@ import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.example.haru.R
 import com.example.haru.viewmodel.TimetableViewModel
 
 
-class ScheduleDraglistener (private val timetableViewModel: TimetableViewModel,
+class ScheduleDraglistener(private val timetableViewModel: TimetableViewModel,
                             layoutIndex: ArrayList<Int>,
                             val context : Context) : View.OnDragListener{
     val layoutIndex = layoutIndex
-    var lastY = 0
     val shadowView = Button(context)
+
     override fun onDrag(view: View, event: DragEvent): Boolean {
-        var targetFrameLayout: FrameLayout
+        val targetFrameLayout = getTargetFrameLayout(view)
+        val draggedView = event.localState as TextView
 
-        if (event.action == DragEvent.ACTION_DROP) {
-            val draggedView = event.localState as View
-            try {
-                targetFrameLayout = view as FrameLayout
-            } catch (e: java.lang.ClassCastException) {
-                targetFrameLayout = view.parent.parent as FrameLayout
-            }
-            val displayMetrics = targetFrameLayout.resources.displayMetrics
-            val x = event.x
-
-//
-//            val y = ((event.y / displayMetrics.density).toInt())
-//            val dropY = Math.round( y * displayMetrics.density)
-
-            val y = ((event.y - draggedView.height / 2) / displayMetrics.density).toInt()
-            val dropY = Math. round( y * displayMetrics. density)
-            draggedView.y = dropY.toFloat()
-
-            val sourceLayout = draggedView.parent as LinearLayout
-
-            Log.d("DRAGGED ACTION_DROP", "${targetFrameLayout.id} , $dropY")
-
-            var index = -1
-            for(i : Int in 0..6){
-                if(targetFrameLayout.id == layoutIndex[i]){
-                    index = i
-                    break
-                }
-            }
-            sourceLayout.removeView(draggedView)
-            timetableViewModel.scheduleMoved(y, draggedView, index)
-        }
-
-        if (event.action == DragEvent.ACTION_DRAG_LOCATION) {
-            val draggedView = event.localState as TextView
-            try {
-                targetFrameLayout = view as FrameLayout
-            } catch (e: java.lang.ClassCastException) {
-                targetFrameLayout = view.parent.parent as FrameLayout
-            }
-            val shadowParams = FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, draggedView.height)
-            shadowView.alpha = 1f
-            shadowView.layoutParams = shadowParams
-            val displayMetrics = targetFrameLayout.resources.displayMetrics
-
-
-//            val y = ((event.y / displayMetrics.density).toInt())
-            val y = ((event.y - draggedView.height / 2) / displayMetrics.density).toInt()
-            val dropY = Math. round( y * displayMetrics. density)
-
-//            val dropY = Math.round( y * displayMetrics.density)
-
-            Log.d("DRAGGED ACTION_DRAG_LOCATION", "${targetFrameLayout.id} , $dropY")
-
-            // Set this new drawable as the background of your View
-            shadowView.background = makeShapeDrawable()
-            shadowView.alpha = 0.5f
-
-            if(dropY != lastY) {
-                try {
-                    val shadowParent = shadowView.parent as FrameLayout
-                    shadowParent.removeView(shadowView)
-                } catch (e: java.lang.NullPointerException){}
-                shadowView.y = dropY.toFloat()
-                shadowView.text = draggedView.text
-                targetFrameLayout.addView(shadowView)
-                true
-            }
-        }
-
-        if (event.action == DragEvent.ACTION_DRAG_EXITED) {
-            try {
-                targetFrameLayout = view as FrameLayout
-            } catch (e: java.lang.ClassCastException) {
-                targetFrameLayout = view.parent.parent as FrameLayout
-            }
-            targetFrameLayout.removeView(shadowView)
+        when (event.action) {
+            DragEvent.ACTION_DROP -> handleDropAction(targetFrameLayout, draggedView, event)
+            DragEvent.ACTION_DRAG_LOCATION -> handleDragLocationAction(targetFrameLayout, draggedView, event)
+            DragEvent.ACTION_DRAG_EXITED -> handleDragExitedAction(targetFrameLayout)
         }
         return true
     }
+
+    private fun handleDropAction(targetFrameLayout: FrameLayout, draggedView: View, event: DragEvent) {
+        val displayMetrics = targetFrameLayout.resources.displayMetrics
+
+        var y = ((event.y - draggedView.height / 2) / displayMetrics.density).toInt()
+        val dropY = Math.round(y * displayMetrics.density)
+
+        val parentBounds = getParentBounds(targetFrameLayout)
+        val newBottom = dropY + draggedView.height
+
+        // Check if the new location is within the parent bounds
+        if (parentBounds.contains(parentBounds.left, dropY, parentBounds.right, newBottom)) {
+            // If it is within bounds, update the location of the view
+            shadowView.y = dropY.toFloat()
+            Log.d("patchMoved", "contain")
+        } else if(dropY <= parentBounds.top){
+            shadowView.y = parentBounds.top.toFloat()
+            Log.d("patchMoved", "dropY <= parentBounds.top")
+        } else if(newBottom > parentBounds.bottom){
+            shadowView.y = (parentBounds.bottom - draggedView.height).toFloat()
+            y = ((parentBounds.bottom - draggedView.height) / displayMetrics.density).toInt()
+
+            Log.d("patchMoved", "newBottom > parentBounds.bottom")
+        }
+
+        Log.d("patchMoved", "patchMoved: ${newBottom}, ${parentBounds.bottom}, ${draggedView.height}, ${shadowView.y}, ${shadowView.y + draggedView.height}")
+        val sourceLayout = draggedView.parent as LinearLayout
+
+        var index = -1
+        for(i : Int in 0..6){
+            if(targetFrameLayout.id == layoutIndex[i]){
+                index = i
+                break
+            }
+        }
+        sourceLayout.removeView(draggedView)
+
+        if(y <= 0) y = 0
+        timetableViewModel.scheduleMoved(y, draggedView, index)
+    }
+
+    private fun handleDragLocationAction(targetFrameLayout: FrameLayout, draggedView: TextView, event: DragEvent) {
+        shadowView.layoutParams = FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, draggedView.height)
+        shadowView.background = makeShapeDrawable()
+        shadowView.alpha = 0.5f
+
+        try {
+            val shadowParent = shadowView.parent as FrameLayout
+            shadowParent.removeView(shadowView)
+        } catch (_: java.lang.NullPointerException) { }
+
+        val displayMetrics = targetFrameLayout.resources.displayMetrics
+        val newLocation = calculateNewLocation(event.y, draggedView.height, displayMetrics)
+        val parentBounds = getParentBounds(targetFrameLayout)
+
+        Log.d("handleDragLocationAction", "handleDragLocationAction: $newLocation, ${parentBounds.top} , ${parentBounds.bottom}")
+
+        updateViewLocation(draggedView, newLocation, parentBounds, targetFrameLayout)
+    }
+
+    private fun handleDragExitedAction(targetFrameLayout: FrameLayout) {
+        targetFrameLayout.removeView(shadowView)
+    }
+
+
+    private fun getTargetFrameLayout(view: View): FrameLayout {
+        return try {
+            view as FrameLayout
+        } catch (e: java.lang.ClassCastException) {
+            view.parent.parent as FrameLayout
+        }
+    }
+
+    private fun calculateNewLocation(y: Float, height: Int, displayMetrics: DisplayMetrics): Int {
+        return Math.round(((y - height / 2) / displayMetrics.density) * displayMetrics.density)
+    }
+
+    private fun getParentBounds(targetFrameLayout: FrameLayout): Rect {
+        return Rect(targetFrameLayout.left, targetFrameLayout.top,
+            targetFrameLayout.right, targetFrameLayout.bottom)
+    }
+
+    private fun updateViewLocation(draggedView: TextView, newLocation: Int, parentBounds: Rect, targetFrameLayout: FrameLayout) {
+        val newTop = newLocation
+        val newBottom = newTop + draggedView.height
+
+        // Check if the new location is within the parent bounds
+        if (parentBounds.contains(parentBounds.left, newTop, parentBounds.right, newBottom)) {
+            // If it is within bounds, update the location of the view
+            shadowView.y = newLocation.toFloat()
+        } else if(newTop < parentBounds.top){
+            shadowView.y = parentBounds.top.toFloat()
+        } else if(newBottom > parentBounds.bottom){
+            shadowView.y = (parentBounds.bottom - draggedView.height).toFloat()
+        }
+        shadowView.text = draggedView.text
+        targetFrameLayout.addView(shadowView)
+    }
+
 
     private fun makeShapeDrawable(): LayerDrawable {
         // Create a GradientDrawable with rounded corners
