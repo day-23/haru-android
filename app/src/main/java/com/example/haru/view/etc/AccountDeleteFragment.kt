1c3a9084556bd173c4880b3e9f7b4183ebec62ad
오홍석 <lkl4502@kookmin.ac.kr>
2023-06-09 00:16:41 +0900

    feature : 계정 삭제 Flow수정 및 디자인 변경

--- app/src/main/java/com/example/haru/view/etc/AccountDeleteFragment.kt
+++ app/src/main/java/com/example/haru/view/etc/AccountDeleteFragment.kt
@@ -1,17 +1,22 @@
 package com.example.haru.view.etc
 
 import BaseActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.example.haru.App
 import com.example.haru.R
 import com.example.haru.databinding.FragmentAccountDeleteBinding
+import com.example.haru.utils.SharedPrefsManager
 import com.example.haru.view.MainActivity
+import com.example.haru.view.auth.LoginActivity
 import com.example.haru.viewmodel.EtcViewModel
 
 class AccountDeleteFragment(val etcViewModel: EtcViewModel) : Fragment() {
@@ -83,13 +88,21 @@ class AccountDeleteFragment(val etcViewModel: EtcViewModel) : Fragment() {
                 }
 
                 binding.btnAccountDelete.id -> {
-                    requireActivity().supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.fragments_frame,
-                            AccountDeleteDoubleCheckFragment(etcViewModel)
-                        )
-                        .addToBackStack(null)
-                        .commit()
+                    etcViewModel.deleteUserAccount {
+                        if (it?.success == true) {
+                            SharedPrefsManager.clear(App.instance)
+                            val intent = Intent(activity, LoginActivity::class.java)
+                            startActivity(intent)
+                            activity?.finish()
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                "계정을 삭제하는데 실패하였습니다..",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.e("20191627", "계정 삭제 실패")
+                        }
+                    }
                 }
             }
         }
