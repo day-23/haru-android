0e0edd84dc190b8d9f212a1eeec5ea936259d0ff
ablecp <tasong12@kookmin.ac.kr>
2023-06-16 11:02:28 +0900

    fix:import 추가

--- app/src/main/java/com/example/haru/view/MainActivity.kt
+++ app/src/main/java/com/example/haru/view/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.haru.view
 import BaseActivity
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.net.Uri
 import android.os.Build
 import android.os.Build.VERSION
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
