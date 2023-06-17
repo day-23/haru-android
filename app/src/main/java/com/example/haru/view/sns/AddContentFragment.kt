477c660db348735146239fd1134b3b713f67dde1
오홍석 <lkl4502@kookmin.ac.kr>
2023-06-18 05:04:28 +0900

    Fix : 하루 그리기, 쓰기 에셋 변경

--- app/src/main/java/com/example/haru/view/sns/AddContentFragment.kt
+++ app/src/main/java/com/example/haru/view/sns/AddContentFragment.kt
@@ -1,10 +1,14 @@
 package com.example.haru.view.sns
 
 import BaseActivity
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.haru.R
 import com.example.haru.data.model.ExternalImages
@@ -17,10 +21,6 @@ class AddContentFragment(
     val select: ArrayList<ExternalImages>
 ) : Fragment() {
     lateinit var binding: FragmentAddContentBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onResume() {
         super.onResume()
@@ -51,6 +51,50 @@ class AddContentFragment(
             transaction.addToBackStack(null)
             transaction.commit()
         }
+
+        binding.addContentText.addTextChangedListener(object :
+            TextWatcher { // 소프트웨어 키보드의 스페이스바 감지
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s == null) {
+                    binding.addContentCancel.background =
+                        ContextCompat.getDrawable(requireContext(), R.drawable.cancel_icon)
+                    binding.addContentCancel.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.date_text
+                        )
+                    )
+                    return
+                }
+
+                val str = s.toString()
+                if (str == ""){
+                    binding.addContentCancel.background =
+                        ContextCompat.getDrawable(requireContext(), R.drawable.cancel_icon)
+                    binding.addContentCancel.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.date_text
+                        )
+                    )
+                    return
+                }
+
+                binding.addContentCancel.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.back_arrow)
+                binding.addContentCancel.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.highlight
+                    )
+                )
+            }
+
+        })
+
         return binding.root
     }
 }
