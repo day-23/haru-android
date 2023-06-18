a9dbabd503c34698343eff7bd578f915cc0afac8
ablecp <tasong12@kookmin.ac.kr>
2023-06-19 03:02:18 +0900

    fix:욕설 감지후 애니메이션 종료

--- app/src/main/java/com/example/haru/view/sns/AddTagFragment.kt
+++ app/src/main/java/com/example/haru/view/sns/AddTagFragment.kt
@@ -100,11 +100,15 @@ class AddTagFragment(
                         transaction.commit()
                     }
                 }else if(done == 429){
-                    Toast.makeText(requireContext(),"글을 너무 자주 작성할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    loading.dismiss {
+                        Toast.makeText(requireContext(),"글을 너무 자주 작성할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }else if(done == 403){
-                    Toast.makeText(requireContext(), "부적절한 단어는 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    loading.dismiss {
+                        Toast.makeText(requireContext(), "부적절한 단어는 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }
-                
+
             }
         }
         binding.addTagPictureIndex.text = "1/${Uris.size}"
